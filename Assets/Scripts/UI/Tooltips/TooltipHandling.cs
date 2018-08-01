1021f21ce2eb3596f2688a91066c0f4c2e677f11
Petter Vilberg <petter.vilberg@gmail.com>
2018-08-01 15:06:26 +0200

    Tooltip screen edge awareness system

--- Assets/Scripts/UI/Tooltips/TooltipHandling.cs
+++ Assets/Scripts/UI/Tooltips/TooltipHandling.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class TooltipHandling : MonoBehaviour {
 
     public bool isOpen = true;
+    //Margins for distances to the edge of the screen
+    public float downwardsMargin = 20f;
+    public float rightwardsMargin = 0f;
     public GameObject textBox;
 
     void Start()
@@ -14,13 +17,43 @@ public class TooltipHandling : MonoBehaviour {
     }
 
     public void Open() {
+        gameObject.GetComponent<TooltipLocation>().SetLocation();
+        CheckEdges();
+        gameObject.GetComponent<TooltipLocation>().SetLocation();
         gameObject.SetActive(true);
         isOpen = true;
+    }
+    
+
+    void CheckEdges()
+    {
+        Vector3[] bottomCheckCorners = new Vector3[4];
+        GameObject.Find("BottomCheck").GetComponent<RectTransform>().GetWorldCorners(bottomCheckCorners);
+        float distanceToCompare = bottomCheckCorners[0].y;
+        Vector3[] ttCorners = new Vector3[4];
+        gameObject.GetComponent<RectTransform>().GetWorldCorners(ttCorners);
+
+        float distanceCheck = ttCorners[3].y - downwardsMargin;
+        if (distanceCheck < distanceToCompare)
+        {
+            gameObject.GetComponent<TooltipLocation>().invertedHeight = true;
+        }
+
+        float rightDistanceCompare = bottomCheckCorners[2].x;
+        float rightDistanceCheck = ttCorners[3].x + rightwardsMargin;
+
+        if (rightDistanceCheck > rightDistanceCompare)
+        {
+            gameObject.GetComponent<TooltipLocation>().invertedWidth = true;
+        }
     }
 
     public void Close()
     {
+        gameObject.GetComponent<TooltipLocation>().invertedHeight = false;
+        gameObject.GetComponent<TooltipLocation>().invertedWidth = false;
         gameObject.SetActive(false);
         isOpen = false;
+        gameObject.GetComponent<TooltipLocation>().SetLocation();
     }
 }
--- Assets/Scripts/UI/Tooltips/TooltipLocation.cs
+++ Assets/Scripts/UI/Tooltips/TooltipLocation.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 public class TooltipLocation : MonoBehaviour {
     
     Vector3 location;
-    Vector3 finalLocation = new Vector3 (-30, 30);
-    public Vector3 offset;
+    public Vector3 finalLocation;
+    Vector3[] mouseSafeZoneCorners = new Vector3[4];
+    public RectTransform mouseSafeZone;
+    public RectTransform tooltipBox;
+    public bool invertedWidth = false;
+    public bool invertedHeight = false;
+    private float xLocation;
+    private float yLocation;
+    public float yPivot;
+    public float xPivot;
 
+    void Start()
+    {
+        tooltipBox = gameObject.GetComponent<RectTransform>();
+        mouseSafeZone = GameObject.Find("MouseSafeZone").GetComponent<RectTransform>();
+    }
 	void Update () {
-        location = Input.mousePosition;
-        finalLocation = location - offset;
-        transform.position = finalLocation;
+        SetLocation();
 	}
+
+    public void SetLocation()
+    {
+        mouseSafeZone.GetWorldCorners(mouseSafeZoneCorners);
+        //Set correct pivots
+        if (invertedHeight)
+        {
+            yPivot = 0;
+            yLocation = mouseSafeZoneCorners[0].y;
+        } else
+        {
+            yPivot = 1;
+            yLocation = mouseSafeZoneCorners[3].y;
+        }
+        if (invertedWidth)
+        {
+            xPivot = 1;
+            xLocation = mouseSafeZoneCorners[0].x;
+        }
+        else
+        {
+            xPivot = 0;
+            xLocation = mouseSafeZoneCorners[3].x;
+        }
+        tooltipBox.pivot = new Vector2(xPivot, yPivot);
+
+        //Actually move the tooltip
+        location = new Vector3(xLocation, yLocation);
+        tooltipBox.transform.position = location;
+    }
 }
